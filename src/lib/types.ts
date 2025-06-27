627505237fde068a3776e9b0f1c45d1fcd4cc16c
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2025-06-28 03:26:24 +0400

    adding some validations

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -3,9 +3,10 @@ export type Currency = (typeof CURRENCIES)[number];
 
 export type Debt = {
   id: string;
-  debtorCreditorName: string;
+  personName: string
   amount: number;
   currency: Currency;
+  type: "owed_to_me" | "i_owe"
   date: Date;
   notes?: string;
 };
---
+++ src/lib/validateSchema/debt.ts
@@ -0,0 +1,13 @@
+import * as z from 'zod';
+import { CURRENCIES } from "../types";
+
+export const debtSchema = z.object({
+  personName: z.string().min(2, 'Name is too short').max(50, 'Name is too long'),
+  amount: z.coerce.number().positive('Amount must be a positive number.'),
+  currency: z.enum(CURRENCIES),
+  type: z.enum(['owed_to_me', 'i_owe']),
+  date: z.date(),
+  notes: z.string().max(280, 'Notes are too long').optional(),
+});
+
+export type DeptValues = z.infer<typeof debtSchema>;
