b9c70a1dd997a28691892bf66d5a6a9af2489455
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2025-06-29 03:31:48 +0400

    adding some more ui

--- src/components/ui/date-picker.tsx
+++ src/components/ui/date-picker.tsx
@@ -13,7 +13,7 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover"
 
-export function DatePickerDemo() {
+export function DatePicker() {
   const [date, setDate] = React.useState<Date>()
 
   return (
